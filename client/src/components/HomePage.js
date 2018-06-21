5058814df8f76163a5188e38e1eb14cc6f3ca207
zperk0 <zperkins9@protonmail.com>
2018-06-21 03:33:18 -0400

    ADDED: Restaurants card

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,25 +3,48 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Map from './Map';
+import RestaurantsCard from './RestaurantsCard';
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {  }
   }
+
+  onButtonClick = () => {
+    console.log('hi');
+  }
+
   render() {
     return (
-      <Map/>
+      <Container>
+        <Map/>
+
+
+        <Content>
+          <Float>
+            <RestaurantsCard/>
+          </Float>
+        </Content>
+
+      </Container>
     );
   }
 }
 
 const Container = styled.div`
-  display: flex;
-  flex: 1;
-  background: red;
   width: 100%;
   height: 100%;
+`;
+
+const Content = Container.extend`
+  position: relative;
+  padding: 16px;
+  pointer-events: none;
+`;
+
+const Float = styled.div`
+  pointer-events: all;
 `;
 
 export default HomePage;
